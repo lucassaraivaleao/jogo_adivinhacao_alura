e2bddffde2295f2f2d3b2bb71ad0960327bc8200
Lucas Leão <saraivaleaolucas@gmail.com>
2022-09-23 15:48:21 -0300

    inserido funcionalidade onde é verificado se o chute foi maior ou menor que o número secreto

--- advinhacao.py
+++ advinhacao.py
@@ -6,8 +6,16 @@ numero_secreto = 42
 chute_str = input('Digite o seu número: ')
 print(f'Você digitou {chute_str}')
 
-if numero_secreto == int(chute_str):
+acertou = int(chute_str) == numero_secreto
+maior = int(chute_str) > numero_secreto
+menor = int(chute_str) < numero_secreto
+
+if acertou:
     print('Você acertou !')
 else:
-    print('Você errou !')
+    if maior:
+        print('Você errou! O seu chute foi maior do que o número secreto')
+    elif menor:
+        print('Você errou! O seu chute foi menor do que o número secreto')
+
 print('Fim do jogo')
