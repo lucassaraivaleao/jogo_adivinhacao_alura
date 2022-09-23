dbbdfa128be041a1fa22f66beb84a31e7e15fca6
Lucas Leão <saraivaleaolucas@gmail.com>
2022-09-22 22:56:31 -0300

    inserção do código que roda o programa de adivinhação

--- advinhacao.py
+++ advinhacao.py
@@ -1,1 +1,13 @@
+print('*********************************')
 print('Bem vindo ao jogo de Advinhação !')
+print('*********************************')
+
+numero_secreto = 42
+chute_str = input('Digite o seu número: ')
+print(f'Você digitou {chute_str}')
+
+if numero_secreto == int(chute_str):
+    print('Você acertou !')
+else:
+    print('Você errou !')
+print('Fim do jogo')
