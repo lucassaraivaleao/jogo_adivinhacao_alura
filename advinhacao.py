2eca920fabce1802199e5208cbf58a64c766f63f
Lucas Leão <saraivaleaolucas@gmail.com>
2022-09-23 16:39:40 -0300

    alterado a sistematica de loop, de while para for.

--- advinhacao.py
+++ advinhacao.py
@@ -3,19 +3,26 @@ print('Bem vindo ao jogo de Advinhação !')
 print('*********************************')
 
 numero_secreto = 42
-chute_str = input('Digite o seu número: ')
-print(f'Você digitou {chute_str}')
+total_de_tentativas = 3
 
-acertou = int(chute_str) == numero_secreto
-maior = int(chute_str) > numero_secreto
-menor = int(chute_str) < numero_secreto
 
-if acertou:
-    print('Você acertou !')
-else:
-    if maior:
-        print('Você errou! O seu chute foi maior do que o número secreto')
-    elif menor:
-        print('Você errou! O seu chute foi menor do que o número secreto')
+for rodada in range(1, total_de_tentativas + 1):
+    print(f'Tentativa {rodada} de {total_de_tentativas}')
+    chute_str = input('Digite o seu número: ')
+    print(f'Você digitou {chute_str}')
+
+    acertou = int(chute_str) == numero_secreto
+    maior = int(chute_str) > numero_secreto
+    menor = int(chute_str) < numero_secreto
+
+    if acertou:
+        print('Você acertou !')
+        break
+    else:
+        if maior:
+            print('Você errou! O seu chute foi maior do que o número secreto')
+        elif menor:
+            print('Você errou! O seu chute foi menor do que o número secreto')
+
 
 print('Fim do jogo')
